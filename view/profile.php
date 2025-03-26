d95b14b9bd9607760068b48432d3aceec009ef24
MarcLopezMolina <marclopezmolina242006@gmail.com>
2025-03-26 09:14:59 +0100

    test

--- view/profile.php
+++ view/profile.php
@@ -1,18 +1,3 @@
-<?php
-
-// inicia la sesión si no está iniciada
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
-
-// Verifica si el user ha iniciado sesión
-if (!isset($_SESSION['user'])) {
-    header('Location: loginUser.php');
-    exit();
-}
-$user = $_SESSION['user'];
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
