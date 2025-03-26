b60ee387c73bc9ae56731910cfc281139c120b47
Jhessyka <jpaloujora@gmail.com>
2025-03-26 08:34:12 +0100

    Added profile.php

--- view/profile.php
+++ view/profile.php
@@ -1,3 +1,23 @@
 <?php
 
+// inicia la sesión si no está iniciada
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+// Verifica si el user ha iniciado sesión
+if (!isset($_SESSION['user'])) {
+    header('Location: loginUser.php');
+    exit();
+}
+$user = $_SESSION['user'];
+
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil de Usuarios</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+ 
