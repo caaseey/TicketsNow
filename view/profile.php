4ad62f108db81e55a2656360594d37218a44051e
MarcLopezMolina <marclopezmolina242006@gmail.com>
2025-03-26 09:12:29 +0100

    please work :,)

--- view/profile.php
+++ view/profile.php
@@ -1,23 +1,11 @@
-<?php
-
-// inicia la sesión si no está iniciada
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
-
-// Verifica si el user ha iniciado sesión
-if (!isset($_SESSION['user'])) {
-    header('Location: loginUser.php');
-    exit();
-}
-$user = $_SESSION['user'];
-
-?>
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perfil de Usuarios</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
- 
+    <title>Document</title>
+</head>
+<body>
+    HOLAAAA
+</body>
+</html>
