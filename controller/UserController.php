9fa1181ee16ce0ef74ea2c0d182fa18ed06cebd7
MarcLopezMolina <marclopezmolina242006@gmail.com>
2025-03-26 08:55:55 +0100

    UserController Login() done!

--- controller/UserController.php
+++ controller/UserController.php
@@ -49,7 +49,7 @@ class UserController
                     $_SESSION['user_surname'] = $user['surname'];
 
                     // Redirección a perfil
-                    header("Location: INTRODUCIR AQUI LA DIRECCIÓN");
+                    header("Location: /Dam/TicketsNow/TicketsNow/view/index.php");
                     exit;
                 } else {
                     $error = "Email o contraseña incorrectos.";
