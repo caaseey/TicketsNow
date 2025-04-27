bc93f35d9fd0f3fa310410f5a68028065006852a
MarcLopezMolina <marclopezmolina242006@gmail.com>
2025-04-27 16:42:22 +0200

    .

--- view/src/php/tickets.php
+++ view/src/php/tickets.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css"/>
-    <link rel="stylesheet" href="css/artist.css"/> 
-    <link rel="stylesheet" href="css/concert.css"/>
+    <link rel="stylesheet" href="../css/index.css"/>
+    <link rel="stylesheet" href="../css/concert.css"/>
+    <link rel="stylesheet" href="../css/tickets.css"/>
     <title>TWICE - Artista</title>
 </head>
 
