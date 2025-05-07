c67686bb31e8867d6eb0e9e8492ef8d667cb1103
MarcLopezMolina <marclopezmolina242006@gmail.com>
2025-05-07 09:30:00 +0200

    Index.php redirects done, Profile pictures save correctly, Keshi concert done

--- view/index.php
+++ view/index.php
@@ -289,7 +289,7 @@
             </div>
             <div class="footer-column">
                 <h3>Sobre nosotros</h3>
-                <a href="../view/src/php/about.php">Haz click aquí</a>
+                <a href="../view/src/html/work_in_progress.html">Haz click aquí</a>
             </div>
             <div class="footer-column">
                 <h3>Llámanos</h3>
@@ -297,7 +297,7 @@
             </div>
             <div class="footer-column">
                 <h3>¿Buscas ayuda?</h3>
-                <a href="../view/src/php/help.php">Página de ayuda</a><br>
+                <a href="../view/src/html/work_in_progress.html">Página de ayuda</a><br>
             </div>
         </div>
 
