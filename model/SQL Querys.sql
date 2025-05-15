ada85ee047eafc224230ea381b33971a21762383
MarcLopezMolina <marclopezmolina242006@gmail.com>
2025-05-15 09:52:51 +0200

    Some changes in SQL Querys File

--- model/SQL Querys.sql
+++ model/SQL Querys.sql
@@ -1,12 +1,27 @@
+-- Aquí pondremos las Querys de MySQL que utilizamos para crear la base de datos.
+-- Si haceis algun cambio en la BBDD, actualizad este archivo.
+
+-- ===============================
+-- 1. CREACIÓN DE LA BASE DE DATOS
+-- ===============================
 CREATE DATABASE ticketsnow; 
 USE ticketsnow; 
 
-CREATE TABLE roles (   
+-- =========================
+-- 2. CREACIÓN DE LAS TABLAS
+-- =========================
+
+CREATE TABLE roles
+(   
 	id_role INT AUTO_INCREMENT NOT NULL PRIMARY KEY,   
 	rol_name VARCHAR(25) NOT NULL
 ); 
 
-CREATE TABLE users (   
+-- Insertar en la tabla roles los diferentes roles de cada usuario. Ejecutar 1 vez y no tocar nada.
+INSERT INTO roles (rol_name) VALUES   ('user'),   ('artist'),   ('admin');
+
+CREATE TABLE users 
+(   
 	id_user INT AUTO_INCREMENT NOT NULL PRIMARY KEY,   
     name VARCHAR(25),   
     surname VARCHAR(25),   
@@ -17,11 +32,12 @@ CREATE TABLE users (
     FOREIGN KEY (id_role) REFERENCES roles(id_role)
 );
     
-    -- Insertar en la tabla roles (¡no en users!) 
-    INSERT INTO roles (rol_name) VALUES   ('user'),   ('artist'),   ('admin');
-    
-    -- Insertar usuarios (no hace falta especificar id_user) 
-    INSERT INTO users (name, surname, email, password, id_role, profile_photo) VALUES   ('casey', 'cleto', 'casey@gmail.com', '123', 1, ''),   ('jonji', 'salango', 'jonji@gmail.com', '123', 2, ''),   ('marc', 'lopez', 'marc@gmail.com', '123', 3, '');
-    
-    -- Verificar 
-    SELECT * FROM users;
+-- Ejemplo de como insertar un usuario en la tabla de users.
+INSERT INTO users (name, surname, email, password, id_role, profile_photo) VALUES   ('casey', 'cleto', 'casey@gmail.com', '123', 1, ''),   ('jonji', 'salango', 'jonji@gmail.com', '123', 2, ''),   ('marc', 'lopez', 'marc@gmail.com', '123', 3, '');
+
+-- ====================
+-- 3. Consultas típicas
+-- ====================
+
+-- Verificar datos en la tabla users.
+SELECT * FROM users;
