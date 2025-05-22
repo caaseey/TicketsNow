45626db31e93f91691165ba0ea96ac78616ca524
caaseey <tu_email@gmail.com>
2025-05-22 14:08:27 +0200

    BBDD

--- model/SQL Querys.sql
+++ model/SQL Querys.sql
@@ -1,20 +1,27 @@
-create database ticketsnow;
+CREATE DATABASE ticketsnow; 
+USE ticketsnow; 
 
-use ticketsnow;
+CREATE TABLE roles (   
+	id_role INT AUTO_INCREMENT NOT NULL PRIMARY KEY,   
+	rol_name VARCHAR(25) NOT NULL
+); 
 
-create table roles(
-id_role int auto_increment not null primary key,
-rol_name varchar(25) not null
+CREATE TABLE users (   
+	id_user INT AUTO_INCREMENT NOT NULL PRIMARY KEY,   
+    name VARCHAR(25),   
+    surname VARCHAR(25),   
+    email VARCHAR(100),   
+    password VARCHAR(255),   
+    id_role INT,   
+    profile_photo VARCHAR(255) NOT NULL,   
+    FOREIGN KEY (id_role) REFERENCES roles(id_role)
 );
-
-create table users(
-id_user int auto_increment not null primary key,
-name varchar(25),
-surname varchar(25),
-email varchar(100),
-password varchar(25),
-id_role int not null,
-FOREIGN KEY (id_role) REFERENCES roles(id_role)
-);
-
-select * from users;
+    
+    -- Insertar en la tabla roles (¡no en users!) 
+    INSERT INTO roles (rol_name) VALUES   ('user'),   ('artist'),   ('admin');
+    
+    -- Insertar usuarios (no hace falta especificar id_user) 
+    INSERT INTO users (name, surname, email, password, id_role, profile_photo) VALUES   ('casey', 'cleto', 'casey@gmail.com', '123', 1, ''),   ('jonji', 'salango', 'jonji@gmail.com', '123', 2, ''),   ('marc', 'lopez', 'marc@gmail.com', '123', 3, '');
+    
+    -- Verificar 
+    SELECT * FROM users;
